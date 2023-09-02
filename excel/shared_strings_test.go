49e4e5947595fe486a39b77f94ae228b5be5ee38
Igor Kolesnik <igor@kolesnik.es>
2023-09-02 19:50:55 +0200

    Shared strings tests
    Combined index tests into a single test.
    
    Added count test.

--- excel/shared_strings_test.go
+++ excel/shared_strings_test.go
@@ -2,15 +2,18 @@ package excel
 
 import "testing"
 
-func Test_indexDistinctItemsAdded(t *testing.T) {
+func Test_StringItemIndex(t *testing.T) {
 	sst := newSharedStrings()
 	table := []struct {
 		text string
 		want int
 	}{
-		{"foo1", 0},
+		{"foo", 0},
 		{"foo2", 1},
 		{"foo3", 2},
+		{"foo", 0},
+		{"foo4", 3},
+		{"foo2", 1},
 	}
 	for _, input := range table {
 		got := sst.add(input.text)
@@ -20,18 +23,25 @@ func Test_indexDistinctItemsAdded(t *testing.T) {
 	}
 }
 
-func Test_indexSameItemsAdded(t *testing.T) {
+func Test_StringItemCount(t *testing.T) {
 	sst := newSharedStrings()
 	table := []struct {
 		text string
 		want int
 	}{
-		{"foo", 0},
-		{"foo", 0},
-		{"foo", 0},
+		{"foo", 3},
+		{"foo", 3},
+		{"foo2", 2},
+		{"foo3", 1},
+		{"foo", 3},
+		{"foo4", 1},
+		{"foo2", 2},
 	}
 	for _, input := range table {
-		got := sst.add(input.text)
+		_ = sst.add(input.text)
+	}
+	for _, input := range table {
+		got := sst.items[input.text].count
 		if input.want != got {
 			t.Errorf("Want: %q, got %q", input.want, got)
 		}
