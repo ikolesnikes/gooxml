b6e98fbc8fb0f808519bd2c34b0169e3cd60d664
Igor Kolesnik <igor@kolesnik.es>
2023-09-03 09:50:58 +0200

    Refactor document saving

--- excel/doc.go
+++ excel/doc.go
@@ -39,22 +39,50 @@ func (doc *Document) addWorkbook() {
 // Save writes the document using the given writer. The written document is the
 // complete (and hopefully valid) .xlsx file.
 func (doc *Document) Save(w io.Writer) error {
-	type partDesc struct {
-		path string
-		part xml.Marshaler
-		body *bytes.Buffer
+	parts := prepare(doc)
+
+	if err := encode(parts); err != nil {
+		return err
 	}
+
+	return write(parts, w)
+}
+
+// Descriptor of a part being saved
+type partDesc struct {
+	path string
+	part xml.Marshaler
+	body *bytes.Buffer
+}
+
+// prepare prepares the document for writing. It generates the content
+// types part and returns a slice of parts for encoding.
+func prepare(doc *Document) []*partDesc {
+	cts := newContentTypes()
+
 	var parts = []*partDesc{
 		{path: "_rels/.rels", part: doc.rels},
-		{path: "xl/workbook.xml", part: doc.wkb},
-		{path: "xl/sharedStrings.xml", part: doc.wkb.sst},
 		{path: "xl/_rels/workbook.xml.rels", part: doc.wkb.rels},
 	}
-	for _, wks := range doc.wkb.sheets {
-		parts = append(parts, &partDesc{path: fmt.Sprintf("xl/worksheets/sheet%d.xml", wks.id), part: wks})
-	}
-	parts = append(parts, &partDesc{path: "[Content_Types].xml", part: makeContentTypes(doc)})
 
+	parts = append(parts, &partDesc{path: "xl/workbook.xml", part: doc.wkb})
+	cts.add(newContentTypeOverride("/xl/workbook.xml", CTWorkbook))
+
+	for _, wks := range doc.wkb.sheets {
+		fName := fmt.Sprintf("sheet%d.xml", wks.id)
+		parts = append(parts, &partDesc{path: fmt.Sprintf("xl/worksheets/%s", fName), part: wks})
+		cts.add(newContentTypeOverride(fmt.Sprintf("/xl/worksheets/%s", fName), CTWorksheet))
+	}
+
+	parts = append(parts, &partDesc{path: "xl/sharedStrings.xml", part: doc.wkb.sst})
+	cts.add(newContentTypeOverride("/xl/sharedStrings.xml", CTSharedStrings))
+
+	parts = append(parts, &partDesc{path: "[Content_Types].xml", part: cts})
+	return parts
+}
+
+// encode encodes each part to XML making them ready for writing.
+func encode(parts []*partDesc) error {
 	// This can be done in parallel
 	var err error
 	for _, part := range parts {
@@ -63,7 +91,11 @@ func (doc *Document) Save(w io.Writer) error {
 			return err
 		}
 	}
+	return nil
+}
 
+// write writes each part out producing the final .xlsx file.
+func write(parts []*partDesc, w io.Writer) error {
 	z := zip.NewWriter(w)
 	for _, part := range parts {
 		f, err := z.Create(part.path)
@@ -76,17 +108,4 @@ func (doc *Document) Save(w io.Writer) error {
 		}
 	}
 	return z.Close()
-}
-
-// makeContentTypes creates the content types part. The part returned
-// is populated with all needed entries based on the given document.
-func makeContentTypes(doc *Document) *contentTypes {
-	cts := newContentTypes()
-	cts.add(newContentTypeOverride("/xl/workbook.xml", CTWorkbook))
-	cts.add(newContentTypeOverride("/xl/sharedStrings.xml", CTSharedStrings))
-	for _, wks := range doc.wkb.sheets {
-		fName := fmt.Sprintf("sheet%d.xml", wks.id)
-		cts.add(newContentTypeOverride(fmt.Sprintf("/xl/worksheets/%s", fName), CTWorksheet))
-	}
-	return cts
 }
